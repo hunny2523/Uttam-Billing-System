bce4f1d83044f402e89590bd40444c0a63a7054f
hunny2523 <honeypatelf302@gmail.com>
2026-03-04 14:07:01 +0530

    Update revision number for index.html in service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-04904475'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.itte9t6o2"
+    "revision": "0.kfq6hv0krl8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
