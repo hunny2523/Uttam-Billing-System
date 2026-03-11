88d8a8f4b4e8a9a1a6d8da5c2f843c71dcb8a324
hunny2523 <honeypatelf302@gmail.com>
2026-03-11 10:34:12 +0530

    Implement item update functionality and refactor item management component

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-04904475'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.i8d2mqetpeg"
+    "revision": "0.2s07ng6ims8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
